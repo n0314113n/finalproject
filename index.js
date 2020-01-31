42b814bad820becb91443094872549a4b5b76a3e
Carl Corsini <carlcorsini@Carls-Mac-mini.local>
2020-01-31 15:22:57 -0800

    added event listener to div 2

--- index.js
+++ index.js
@@ -1,48 +1,49 @@
 window.onload = function() {
-    // necessary for dropdown menus to function properly:
-    // https://stackoverflow.com/a/25347431
-    $('.ui.dropdown').dropdown();
+  // necessary for dropdown menus to function properly:
+  // https://stackoverflow.com/a/25347431
+  $('.ui.dropdown').dropdown();
 
-    //grab button with query selector
-    let btn = document.querySelector('#clickMe')
+  //grab button with query selector
+//   let btn = document.querySelector('#clickMe');
 
-    //grab thing to change with query selector
-    let changeMe = document.querySelector('#changeMe')
+//   //grab thing to change with query selector
+//   let changeMe = document.querySelector('#changeMe');
 
-    //add event listener to button
-    btn.addEventListener('mouseover', function() {
+//   //add event listener to button
+//   btn.addEventListener('mouseover', function() {
+//     console.log('You did it!');
 
-        console.log('You did it!')
+//     let par = document.createElement('p');
 
-        let par = document.createElement('p')
+//     par.innerHTML = 'New Thing';
 
-        par.innerHTML = 'New Thing'
+//     changeMe.innerHTML = 'Hello';
+//     changeMe.style.fontSize = 30;
+//     changeMe.style.color = 'white';
+//     changeMe.style.background = 'black';
 
-        changeMe.innerHTML = 'Hello';
-        changeMe.style.fontSize = 30;
-        changeMe.style.color = 'white';
-        changeMe.style.background = 'black';
+//     changeMe.appendChild(par);
+//   });
+//   console.log('click');
 
-        changeMe.appendChild(par)
+  //on 'event' change thing to other thing
 
-    })
-    console.log('click')
+  $('.sequenced.images .image').transition({
+    animation: 'scale',
+    reverse: 'auto', // default setting
+    interval: 200,
+  });
 
-    //on 'event' change thing to other thing
+  // Carl Edits
+  // ********************
+  // ********************
+  // ********************
 
+  // select click 2 div
+  let click2 = document.querySelector('#click2');
 
-
-    $('.sequenced.images .image')
-        .transition({
-            animation: 'scale',
-            reverse: 'auto', // default setting
-            interval: 200
-        });
-
-
-
-
-
-
-
-}
+  // when div is clicked do something!
+  click2.addEventListener('click', function(){
+      console.log('clicked 2!')
+  })
+};
