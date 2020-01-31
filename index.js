9172ee78f9b0609dbed54ff8860ee10bb0172cfd
Owner <owner@allen.local>
2020-01-30 20:42:33 -0800

    div party

---
+++ index.js
@@ -0,0 +1,43 @@
+window.onload = function() {
+    // necessary for dropdown menus to function properly:
+    // https://stackoverflow.com/a/25347431
+    $('.ui.dropdown').dropdown();
+
+    //grab button with query selector
+    let btn = document.querySelector('#clickMe')
+
+    //grab thing to change with query selector
+    let changeMe = document.querySelector('#changeMe')
+
+    //add event listener to button
+    btn.addEventListener('mouseover', function() {
+
+        console.log('You did it!')
+
+        let par = document.createElement('p')
+
+        par.innerHTML = 'New Thing'
+
+        changeMe.innerHTML = 'Hello';
+        changeMe.style.fontSize = 30;
+        changeMe.style.color = 'white';
+        changeMe.style.background = 'black';
+
+        changeMe.appendChild(par)
+
+    })
+    console.log('click')
+
+    //on 'event' change thing to other thing
+
+
+
+
+
+
+
+
+
+
+
+}
